436a80f05644a42db60b84c2ab5739bc09080073
asifZaman0362 <zero362001@gmail.com>
2020-02-07 13:04:33 +0530

    Everything works properly

--- gallery.js
+++ gallery.js
@@ -1,31 +1,49 @@
-var prev = 0;
-var toRemove = 0;
+// Get all image filenames in an array.
+// You can make it automatic by using a http server request returning an array using the readdir() function.
+var images = ["image","image2","image3","image4","image5"];
+var path = "res/gallery_img/"; // Path to directory containing images
+var extension = ".jpg"; // The file extension for images
 
-setInterval(doRefresh, 4000);
+var image_front, image_back = null;
+
+window.addEventListener("load", onDocumentLoad);
+
+function onDocumentLoad() {
+    console.log("document loaded!");
+    // Find our image view elements
+    image_back = document.getElementById("s0");
+    image_front = document.getElementById("s1");
+    // Set the first image so it's not blank and weird
+    image_back.src = getImageFromFile(0);
+    // Update the image every 4 seconds (4000 miliseconds)
+    setInterval(updateImage, 4000);
+}
+
+// Get the indexth image from the array by joining the proper paths
+function getImageFromFile(index) {
+    return path + images[index] + extension;
+}
 
 var curr = 0;
 
-function doRefresh() {
-    if (curr == 4) curr = 0;
+function updateImage() {
+    console.log("updating image...");
+    if (curr == images.length - 1) curr = 0;
     else curr++;
-    getImage(curr); 
+    setImage(curr);
 }
-function getImage(index) {
-    if (index == prev) {
-        console.log("Image already showing!");
-        return;
-    } else {
-        console.log("Pulling in image "+index+" and removing image "+prev);
-        var newImg = document.getElementById("s"+index);
-        newImg.style.left = "0px";
-        toRemove = prev;
-        setTimeout(() => hideImage(toRemove), 1000);
-        prev = index;
-    }
+
+function setImage(index) {
+    console.log("setting new image...");
+    image_front.src = getImageFromFile(index);
+    image_front.style.transition = "1s";
+    image_front.style.left = "0px";
+    setTimeout(() => slideBack(index), 1050);
 }
-function hideImage(index) {
-    console.log("dafuq!");
-    document.getElementById("s"+index).classList.remove("slide");
-    document.getElementById("s"+index).style.left = "-1366px";
-    document.getElementById("s"+index).classList.add("slide");
+
+function slideBack(index) {
+    console.log("new image set.");
+    image_back.src = getImageFromFile(index);
+    image_front.style.transition = "none";
+    image_front.style.left = "-1366px";
 }
